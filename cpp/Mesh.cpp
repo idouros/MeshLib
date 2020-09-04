3f69f25449f1d9d1b9efec6a5dff23129414172c
Yannis Douros <yannis@douros.org>
2020-09-04 14:32:10 +0300

    Better class design.

--- cpp/Mesh.cpp
+++ cpp/Mesh.cpp
@@ -1,6 +1,52 @@
 #include "Mesh.h"
 
-int MeshCode()
+void GeoMesh::SaveAsObjFile(std::ofstream& outfile)
 {
-    return 666;
+	auto rows = this->altitudes.size();
+	auto cols = this->altitudes[0].size();
+
+	// vertices
+	for (auto row = 0; row < rows; row++)
+	{
+		for (auto col = 0; col < cols; col++)
+		{
+			outfile << "v " << row * this->params.scale << " " << col * this->params.scale << " " << this->altitudes[row][col] << std::endl;
+		}
+	}
+
+	// faces
+	for (auto row = 0; row < rows - 1; row++)
+	{
+		for (auto col = 0; col < cols - 1; col++)
+		{
+			auto top_left = (row * cols) + col + 1;
+			auto top_right = top_left + 1;
+			auto bottom_left = top_left + cols;
+			auto bottom_right = bottom_left + 1;
+
+			// first triangle
+			outfile << "f " << top_left << " " << bottom_left << " " << top_right << std::endl;
+			// second triangle
+			outfile << "f " << top_right << " " << bottom_left << " " << bottom_right << std::endl;
+		}
+	}
+}
+
+GeoMesh::GeoMesh(const LandscapeParams& parameters)
+{
+	this->params = parameters;
+
+	// YYY
+	this->altitudes = {
+		{ 0, 0, 0 ,0 },
+		{ 0, 10, 0, 0 },
+		{ 0, 0, 0 ,0 },
+		{ 0, 0, 0 ,0 }
+	};
+}
+
+std::shared_ptr<GeoMesh> GeoMesh::CreateGeoMesh(const LandscapeParams& parameters)
+{
+	auto gm = GeoMesh(parameters);
+	return std::make_shared<GeoMesh>(gm);
 }
--- inc/Mesh.h
+++ inc/Mesh.h
@@ -4,41 +4,28 @@
 #include<vector>
 #include<fstream>
 
-using namespace std;
-
 // TODO make this a class
+
 typedef std::vector<std::vector<double>> GridMesh;
+typedef std::vector<std::vector<bool>> FlagMesh;
 
-void SaveAsObjFile(const GridMesh& m, const double& scale, std::ofstream& outfile)
+struct LandscapeParams {
+	double scale;
+};
+
+class GeoMesh
 {
-	auto rows = m.size();
-	auto cols = m[0].size();
+public:
+	void SaveAsObjFile(std::ofstream& outfile);
 
-	// vertices
-	for (auto row = 0; row < rows; row++)
-	{
-		for (auto col = 0; col < cols; col++)
-		{
-			outfile << "v " << row * scale << " " << col * scale << " " << m[row][col] << std::endl;
-		}
-	}
+	// Factory
+	static std::shared_ptr<GeoMesh> CreateGeoMesh(const LandscapeParams& parameters);
 
-	// faces
-	for (auto row = 0; row < rows-1; row++)
-	{ 
-		for (auto col = 0; col < cols-1; col++)
-		{
-			auto top_left = (row * cols) + col + 1;
-			auto top_right = top_left + 1;
-			auto bottom_left = top_left + cols;
-			auto bottom_right = bottom_left + 1;
-
-			// first triangle
-			outfile << "f " << top_left  << " " << top_right  << " " << bottom_left << std::endl;
-			// second triangle
-			outfile << "f " << top_right  << " " << bottom_right  << " " << bottom_left << std::endl;
-		}
-	}
-}
+private:
+	GeoMesh(const LandscapeParams& parameters);
+	GridMesh altitudes;
+	FlagMesh setFlags;
+	LandscapeParams params;
+};
 
 #endif
