572a10ee3d4796bfaafd36ddaeda4aaa6198886f
Yannis Douros <yannis@douros.org>
2020-09-04 16:36:15 +0300

    This definitely looks like landscapes already.

--- cpp/Mesh.cpp
+++ cpp/Mesh.cpp
@@ -1,4 +1,5 @@
 #include "Mesh.h"
+#include <time.h>
 
 void GeoMesh::SaveAsObjFile(std::ofstream& outfile)
 {
@@ -10,7 +11,7 @@ void GeoMesh::SaveAsObjFile(std::ofstream& outfile)
 	{
 		for (auto col = 0; col < cols; col++)
 		{
-			outfile << "v " << row * this->params.scale << " " << col * this->params.scale << " " << this->altitudes[row][col] << std::endl;
+			outfile << "v " << row * this->params.grid_scale << " " << col * this->params.grid_scale << " " << this->altitudes[row][col] << std::endl;
 		}
 	}
 
@@ -36,17 +37,95 @@ GeoMesh::GeoMesh(const LandscapeParams& parameters)
 {
 	this->params = parameters;
 
-	// YYY
-	this->altitudes = {
-		{ 0, 0, 0 ,0 },
-		{ 0, 10, 0, 0 },
-		{ 0, 0, 0 ,0 },
-		{ 0, 0, 0 ,0 }
-	};
+	auto rows = this->params.grid_rows;
+	auto cols = this->params.grid_cols;
+
+	this->altitudes.resize(rows, std::vector<double>(cols, 0.0));
+	this->setFlags.resize(rows, std::vector<bool>(cols, false));
+
+	this->FillRegion(0, 0, rows - 1, cols - 1);
 }
+
+double GeoMesh::RandomAltitude(const double& min_altitude, const double& max_altitude)
+{
+	double r = rand() % 10000;
+	return min_altitude + (max_altitude - min_altitude) * (r / 10000.0);
+}
+
+
+void GeoMesh::CheckAndSetRandomAltitude(
+	const size_t& row,
+	const size_t& col,
+	const std::vector<double>& v = {}
+)
+{
+	double min_altitude = std::numeric_limits<double>::max();
+	double max_altitude = std::numeric_limits<double>::min();
+
+	if (v.empty())
+	{
+		min_altitude = this->params.min_altitude;
+		max_altitude = this->params.max_altitude;
+	}
+	else
+	{
+		for (int i = 0; i < v.size(); i++)
+		{
+			if (v[i] > max_altitude)
+			{
+				max_altitude = v[i];
+			}
+			if (v[i] < min_altitude)
+			{
+				min_altitude = v[i];
+			}
+		}
+	}
+
+	if (!(this->setFlags[row][col]))
+	{
+		this->altitudes[row][col] = this->RandomAltitude(min_altitude, max_altitude);
+		this->setFlags[row][col] = true;
+	}
+}
+
+void GeoMesh::FillRegion(const size_t& start_row, const size_t& start_col, const size_t& end_row, const size_t& end_col)
+{
+	if ((end_row > start_row + 1) || (end_col > start_col + 1))
+	{
+		auto mid_row = (start_row + end_row) / 2;
+		auto mid_col = (start_col + end_col) / 2;
+
+		// Fill corners
+		this->CheckAndSetRandomAltitude(start_row, start_col);
+		this->CheckAndSetRandomAltitude(start_row, end_col);
+		this->CheckAndSetRandomAltitude(end_row, start_col);
+		this->CheckAndSetRandomAltitude(end_row, end_col);
+		
+		// Fill sides
+		this->CheckAndSetRandomAltitude(start_row, mid_col, { altitudes[start_row][start_col],	altitudes[start_row][end_col] });
+		this->CheckAndSetRandomAltitude(end_row, mid_col, { altitudes[end_row][start_col],		altitudes[end_row][end_col] });
+		this->CheckAndSetRandomAltitude(mid_row, start_col, { altitudes[start_row][start_col],	altitudes[end_row][start_col] });
+		this->CheckAndSetRandomAltitude(mid_row, end_col, { altitudes[start_row][end_col],		altitudes[end_row][end_col] });
+
+		// Fill middle
+		this->CheckAndSetRandomAltitude(mid_row, mid_col, { altitudes[start_row][end_col], altitudes[end_row][end_col], altitudes[start_row][start_col], altitudes[end_row][start_col] });
+
+		// Recurse
+		this->FillRegion(start_row, start_col, mid_row, mid_col);
+		this->FillRegion(mid_row, start_col, end_row, mid_col);
+		this->FillRegion(start_row, mid_col, mid_row, end_col);
+		this->FillRegion(mid_row, mid_col, end_row, end_col);
+	}
+}
+
+
+
+
 
 std::shared_ptr<GeoMesh> GeoMesh::CreateGeoMesh(const LandscapeParams& parameters)
 {
 	auto gm = GeoMesh(parameters);
+	srand(time(NULL)); // TODO - better randomization
 	return std::make_shared<GeoMesh>(gm);
 }
--- inc/Mesh.h
+++ inc/Mesh.h
@@ -10,7 +10,12 @@ typedef std::vector<std::vector<double>> GridMesh;
 typedef std::vector<std::vector<bool>> FlagMesh;
 
 struct LandscapeParams {
-	double scale;
+	size_t grid_rows;
+	size_t grid_cols;
+	double grid_scale;
+	double max_altitude;
+	double min_altitude;
+	double variance;
 };
 
 class GeoMesh
@@ -23,6 +28,16 @@ public:
 
 private:
 	GeoMesh(const LandscapeParams& parameters);
+
+	double RandomAltitude(const double& min_altitude, const double& max_altitude);
+	void CheckAndSetRandomAltitude(
+		const size_t& row,
+		const size_t& col,
+		const std::vector<double>& v
+	);
+
+	void FillRegion(const size_t& start_row, const size_t& start_col, const size_t& end_row, const size_t& end_col);
+
 	GridMesh altitudes;
 	FlagMesh setFlags;
 	LandscapeParams params;
